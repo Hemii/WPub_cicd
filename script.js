bda3cd9cffb4842d90a5b3b3e7a4cd96f2c3deb5
H3MII <rusnak.anton3@gmail.com>
2020-05-21 17:13:20 +0200

    first deploy

---
+++ script.js
@@ -0,0 +1,42 @@
+new Vue({ 
+
+  el: "#app",
+  data: {
+    CORS:'https://api.rss2json.com/v1/api.json?rss_url=',
+    items: [],
+    feed: "https://www.sme.sk/rss-title" 
+  },
+
+  created: function () {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData: function () {
+      this.$http.get(this.CORS + this.feed).then(response => {
+        this.items = response.body;
+        
+      });
+    },
+   
+    changeHandler: function (event) {
+      this.feed = event.target.value;
+      this.fetchData();
+    },
+    clicked: function () {
+      console.log(this.items);
+    },
+    
+    sortDesc () {
+      this.items.items.sort(function(a,b){
+        return new Date(b.pubDate) - new Date(a.pubDate);
+      });
+    },
+    sortAsc () {
+        this.items.items.sort(function(a,b){
+          return new Date(b.pubDate) - new Date(a.pubDate);
+        }).reverse();
+    }
+  
+  } 
+});
